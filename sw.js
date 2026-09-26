edabc61b84ff5ebb00eca5317ec3c3370701c80c
congregacaojardimcaiuby-gif <congregacaojardimcaiuby@gmail.com>
2026-09-25 21:51:18 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'cong-v1';
-const SHELL = ['./','./index.html','./manifest.webmanifest'];
+const CACHE = 'cong-v2';
+const SHELL = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
